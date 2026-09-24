c2480e2f1809bb66ae7d21b7907338d8e9d94a4a
francisleonard612-coder <francisleonard612@gmail.com>
2026-09-24 09:16:07 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -63,7 +63,7 @@ class Config:
     symbols: List[str] = field(
         default_factory=lambda: _env_list(
             "SYMBOLS",
-            ["R_10", "R_25", "R_50", "R_75", "R_100"],
+            ["R_10", "1HZ10V"],
         )
     )
 
